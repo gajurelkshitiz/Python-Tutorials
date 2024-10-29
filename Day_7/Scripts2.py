a69dd8b208dd8b7142b18ac4ac8587ca2b5f301c
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2024-10-29 19:11:38 +0545

    Day-7

---
+++ Day_7/Scripts2.py
@@ -0,0 +1,9 @@
+from Scripts1 import *
+
+def favorite_drink(drink):
+    print(f"Your favorite drink is {drink}")
+    
+print("This is Script2")
+favorite_food("Sushi")
+favorite_drink("Whisky")
+print("Byee")
---
+++ Day_7/main.py
@@ -0,0 +1,13 @@
+import example 
+
+#print(example.pi)
+
+
+result = example.square(3)
+result = example.cube(3)
+result = example.cricumference(3)
+result = example.area(3)
+print(result)
+
+
+print(__name__)
