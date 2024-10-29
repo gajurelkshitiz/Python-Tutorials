c13da75a62b8bd6468a806eaeb5c18d26d9a3372
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2024-10-29 21:23:21 +0545

    End of Day-7

--- Day_7/Scripts2.py
+++ Day_7/Scripts2.py
@@ -3,7 +3,11 @@ from Scripts1 import *
 def favorite_drink(drink):
     print(f"Your favorite drink is {drink}")
     
-print("This is Script2")
-favorite_food("Sushi")
-favorite_drink("Whisky")
-print("Byee")
+def main():
+    print("This is Script2")
+    favorite_food("Sushi")
+    favorite_drink("Whisky")
+    print("Byee")
+
+if __name__ == "__main__":
+    main()
