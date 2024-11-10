87d6a6ee713e2e8e9ccf8e1b664c49149ff4e695
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2024-11-10 11:27:25 +0545

    PyQt5 GUI Intro

---
+++ PyQt5 GUI/creating_window.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("First GUI App")
+        self.setWindowIcon(QIcon('PyQt5 GUI/icon.jpg'))
+        self.setGeometry(500, 300, 700, 500)
+        
+        
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
+    
